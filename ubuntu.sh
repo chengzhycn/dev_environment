81e3333a64b6709089038bc78dd9135670f2ec7a
chengzhycn <chengzhycn@gmail.com>
2021-01-08 13:59:12 +0800

    update ubuntu.sh
    Signed-off-by: chengzhycn <chengzhycn@gmail.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ CARGO=$(command -v cargo 2>/dev/null)
 CARGO_TOOLS=(starship fd-find exa bat)
 
 function tools() {
-    sudo apt install shellcheck lua5.3 jq git make curl -y
+    sudo apt install zsh shellcheck lua5.3 jq git make curl -y
     return $?
 }
 
@@ -54,6 +54,13 @@ registry = "https://mirrors.tuna.tsinghua.edu.cn/git/crates.io-index.git"
 [source.rustcc]
 registry = "https://code.aliyun.com/rustcc/crates.io-index.git"
 EOF
+
+    source "${HOME}"/.cargo/env
+    CARGO=$(command -v cargo 2>/dev/null)
+    if [[ "${CARGO}" == "" ]]; then
+        echo "error install rust"
+        return 1
+    fi
 }
 
 function rust_tools() {
@@ -67,7 +74,35 @@ function rust_tools() {
     done
 }
 
+function zsh() {
+    if ! command -v zsh >/dev/null 2>&1; then
+        echo "zsh is not installed, install it first"
+        return 1
+    fi
+
+    # oh-my-zsh
+    curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o /tmp/install.sh ||
+        {
+            echo "curl oh-my-zsh install script failed."
+            return 1
+        }
+
+    bash /tmp/install.sh --unattended ||
+        {
+            rm -rf /tmp/install.sh
+            echo "install oh-my-zsh failed."
+            return 1
+        }
+
+    rm -rf /tmp/install.sh
+
+    # apply starship prompt
+    echo 'eval "$(starship init zsh)"' >> "${HOME}"/.zshrc
+    # source "${HOME}"/.zshrc
+}
+
 apt_source
 tools || exit 1
 rust || exit 1
 rust_tools || exit 1
+zsh || exit 1
